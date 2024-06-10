0d6a6eb7295234f4c3b971fcc653fa2c01742c7e
Peshang <peshang72@outlook.com>
2024-06-10 13:26:23 +0300

    Added a function for playing 1 round.

--- script.js
+++ script.js
@@ -10,5 +10,50 @@ function getComputerChoice() {
     }
 }
 function getHumanChoice() {
-    return humanChoice = prompt("Let's play Rock Paper Scissors!");
+    return requestedChoice = prompt("Let's play Rock Paper Scissors!");
+}
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice.toLowerCase()) {
+        console.log(`The computer's choice is ${computerChoice}`);
+        console.log("It's a Tie!");
+    }
+    else if (humanChoice === "rock" && computerChoice === "Paper") {
+        console.log(`The computer's choice is ${computerChoice}`);
+        console.log("You lost this round!");
+        computerScore++;
+    }
+    else if (humanChoice === "rock" && computerChoice === "Scissors") {
+        console.log(`The computer's choice is ${computerChoice}`);
+        console.log("Congrats! You won!");
+        humanScore++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "Rock") {
+        console.log(`The computer's choice is ${computerChoice}`);
+        console.log("You lost!");
+        computerScore++;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "Paper") {
+        console.log(`The computer's choice is ${computerChoice}`);
+        console.log("Congrats! You won!");
+        humanScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "Scissors") {
+        console.log(`The computer's choice is ${computerChoice}`);
+        console.log("You lost!");
+        computerScore++;
+    }
+    else if (humanChoice === "paper" && computerChoice === "Rock") {
+        console.log(`The computer's choice is ${computerChoice}`);
+        console.log("Congrats! You won!");
+        humanScore++;
+    }
+    else {
+        console.log("Invalid Input!");
+    }
+
 }
